b1288adcf9a617c18c07523ae19cd73d9eccb8a3
Mathieu <duguet@simplon-charleville.fr>
2020-06-19 15:42:29 +0200

    version stable

---
+++ includes/navbar.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div id="logo">
+        <img src="images/logo.png" alt="logo-du-garage">
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Vente de produit Auto</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Vente de véhicule</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+        <hr/>
+        <ul>
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-instagram"></i>
+        </ul>
+    </div>
+</nav>
